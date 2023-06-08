9cddef0f822930be3c7317a2d4a7fd54612f548e
Bogdan <bogdanjsd@gmail.com>
2023-06-09 00:50:47 +0200

    update to keyboard controls

--- arena/Arena.gd
+++ arena/Arena.gd
@@ -1,64 +1,74 @@
 extends Node2D
 
-func _ready():
-	Global.connect("restart",self,"restart")
-	restart()
-	voda_start.append($VodaLevo.global_position)
-	voda_start.append($VodaDesno.global_position)
-	
-	Global.connect("gameover",self,"game_over")
-	Global.connect("roundstart",self,"round_start")
-
-var mojsije_timer: float = 23.0
 
 var voda_start = []
 
-var fish_timer: float
+var mojsije_timer: Timer
+var fish_timer: Timer
 var fish_cd: float = 7.0
+
 var game_active: bool = false
 
+
+func _ready():
+	
+	mojsije_timer = Timer.new()
+	add_child(mojsije_timer)
+	mojsije_timer.connect("timeout",self,"trigger_mojsije")
+	
+	fish_timer = Timer.new()
+	add_child(fish_timer)
+	fish_timer.connect("timeout",self,"trigger_fish")
+	
+	Global.connect("restart",self,"restart")
+	Global.connect("gameover",self,"game_over")
+	Global.connect("roundstart",self,"round_start")
+	
+	voda_start.append($VodaLevo.global_position)
+	voda_start.append($VodaDesno.global_position)
+	
+	restart()
+	
+
 func game_over():
+	stop_timers()
 	game_active = false
 
 func round_start():
+	start_timers()
 	game_active = true
 
-func _process(delta):
-	if not game_active: return
-	mojsije_timer -= delta
-	if mojsije_timer <= 0:
-		mojsije_timer += 36.0 + randf()*12
-		trigger_mojsije()
-	fish_timer -= delta
-	if fish_timer <= 0:
-		fish_timer += fish_cd
-		var fish = preload("res://fish/fishpickup.tscn").instance()
-		fish.fish = [preload("res://fish/swordfish.tres")][randi()%1]
-		var x = 1920/2 -250 + 500*randf()
-		fish.global_position = Vector2(x,-100)
-		fish.get_node("Riba1").texture = fish.fish.pickup_texture
-		add_child(fish)
+func start_timers():
+	mojsije_timer.start(23.0)
+	fish_timer.start(fish_cd)
+
+func stop_timers():
+	mojsije_timer.stop()
+	fish_timer.stop()
 
 func restart():
-	mojsije_timer = 20.0
 	var fight321 = preload("res://ui/321.tscn").instance()
 	add_child(fight321)
+	
 	if mojsije_tween:
 		mojsije_tween.stop()
-	if voda_start.size() > 0:
-		$VodaLevo.global_position = voda_start[0]
-		$VodaDesno.global_position = voda_start[1]
+		
+	$VodaLevo.global_position = voda_start[0]
+	$VodaDesno.global_position = voda_start[1]
 	$VodaDesno/MojsijePutic.modulate.a = 0.0
 	$VodaLevo/MojsijePutic.modulate.a = 0.0
+	
 	$Neptun.global_position = Vector2(1920,1080)/2.0 + Vector2(500,0)
 	$Poseidon.global_position = Vector2(1920,1080)/2.0 + Vector2(-500,0)
 	$Neptun.restart()
 	$Poseidon.restart()
+	
 	Global.mojsije_split = false
 
 var mojsije_tween: SceneTreeTween
 
 func trigger_mojsije():
+	mojsije_timer.wait_time = 36.0 + randf()*8.0
 	var incoming = preload("res://ui/mojsijeincoming.tscn").instance()
 	add_child(incoming)
 	yield(incoming,"done")
@@ -80,6 +90,14 @@ func trigger_mojsije():
 	mojsije_tween.chain()
 	mojsije_tween.tween_property($VodaLevo/MojsijePutic,"modulate:a",0.0,0.6)
 	mojsije_tween.tween_property($VodaDesno/MojsijePutic,"modulate:a",0.0,0.6)
+	
+func trigger_fish():
+	var fish = preload("res://fish/fishpickup.tscn").instance()
+	fish.fish = [preload("res://fish/swordfish.tres")][randi()%1]
+	var x = 1920/2 -250 + 500*randf()
+	fish.global_position = Vector2(x,-100)
+	fish.get_node("Riba1").texture = fish.fish.pickup_texture
+	add_child(fish)
 	
 func mojsije_walk():
 	var mojsije = preload("res://characters/mojsije/mojsije.tscn").instance()
--- ui/menu/mainmenu.gd
+++ ui/menu/mainmenu.gd
@@ -1,22 +1,10 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.play_music(preload("res://sfx/menumusic.mp3"))
 	$VBoxContainer/Buttons/Play.grab_focus()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+	
 func _on_Play_pressed():
 	Global.play_sound(preload("res://sfx/menu_click_SFX_2.wav"))
 	get_tree().change_scene("res://arena/Arena.tscn")
